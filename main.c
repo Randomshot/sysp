cc574e2cc21b07fbe386d7e3a0728663b5a48805
Randomshot <sksghgus7175@gmail.com>
2020-12-14 21:20:18 +0900

    sample

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main(void){
+	return 0;
+}
